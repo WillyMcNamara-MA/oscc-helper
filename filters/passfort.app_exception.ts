dac8e695e3630b8ab70d9c377cddaf909961c4f9
Willy McNamara <willy.mcnamara@moodys.com>
2024-06-04 13:09:24 -0600

    Adding filtering types and logic for further proof of concept

---
+++ filters/passfort.app_exception.ts
@@ -0,0 +1,15 @@
+import { AppException } from '../types/passfort.app_exception.types';
+import { ErrorResponse, IntegrationError } from '../types/passfort.error_response.types';
+
+export const handleAppException = (exception: AppException) => {
+    const errors: IntegrationError[] = [{
+      type: exception.passfortErrorCode,
+      message: exception.message,
+    }];
+    const warnings = [];
+  
+    return {
+      statusCode: exception.statusCode,
+      errorResponse: new ErrorResponse(errors, warnings),
+    };
+  }
--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-export function test(a: string): string {
-  return `test, here is argument: ${a}`
-}
+export * from './filters/passfort.app_exception';
+export * from './types/passfort.app_exception.types';
+export * from './types/passfort.error_response.types';
