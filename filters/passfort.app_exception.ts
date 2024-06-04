7fe62c6a48e8ec4961a10a8a2343155d25bbe7a8
Willy McNamara <willy.mcnamara@moodys.com>
2024-06-04 13:16:06 -0600

    Add explicit return type in app_exception

--- filters/passfort.app_exception.ts
+++ filters/passfort.app_exception.ts
@@ -1,7 +1,7 @@
 import { AppException } from '../types/passfort.app_exception.types';
 import { ErrorResponse, IntegrationError } from '../types/passfort.error_response.types';
 
-export const handleAppException = (exception: AppException) => {
+export const handleAppException = (exception: AppException): {statusCode: number, errorResponse: ErrorResponse} => {
     const errors: IntegrationError[] = [{
       type: exception.passfortErrorCode,
       message: exception.message,
