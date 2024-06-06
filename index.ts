7f14d7f0493870583160b6b48ea71a39e4327927
Willy McNamara <willy.mcnamara@moodys.com>
2024-06-06 17:04:56 -0600

    Rework exports to skirt bug

--- index.ts
+++ index.ts
@@ -4,5 +4,5 @@ export * from './types/OTS_CC_CheckResponse';
 export * from './types/passfort.signature_validation.types';
 export * from './formatters/OTS_CC_helpers';
 export * from './formatters/validate_signature.helpers';
-export * from './formatters/auth_token.helpers';
 export * from './filters/passfort.app_exception';
+export * from './formatters/auth.helpers';
