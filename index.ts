86cdb29112ea19aae506cdff1a42b51d95d490be
Willy McNamara <willy.mcnamara@moodys.com>
2024-06-06 16:58:28 -0600

    correct the export path

--- index.ts
+++ index.ts
@@ -5,4 +5,4 @@ export * from './types/OTS_CC_CheckResponse';
 export * from './formatters/OTS_CC_helpers';
 export * from './formatters/validate_signature.helpers';
 export * from './types/passfort.signature_validation.types';
-export * from './formatters/validate_signature.helpers';
+export * from './formatters/auth_token.helpers';
