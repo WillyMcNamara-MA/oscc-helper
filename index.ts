83e81c4b6a225b8a219137022d040619e48194dc
Willy McNamara <willy.mcnamara@moodys.com>
2024-06-06 16:56:50 -0600

    bumps version, adds export

--- index.ts
+++ index.ts
@@ -4,4 +4,5 @@ export * from './types/passfort.error_response.types';
 export * from './types/OTS_CC_CheckResponse';
 export * from './formatters/OTS_CC_helpers';
 export * from './formatters/validate_signature.helpers';
-export * from './types/passfort.signature_validation.types'
+export * from './types/passfort.signature_validation.types';
+export * from './formatters/validate_signature.helpers';
