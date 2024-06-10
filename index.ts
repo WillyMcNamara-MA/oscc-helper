f2ee013d4df703a42f4fa7294b25e7dfdf2a8785
Willy McNamara <willy.mcnamara@moodys.com>
2024-06-10 11:33:23 -0600

    Adds types to build a one time sync, company custom check config

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ export * from './types/passfort.app_exception.types';
 export * from './types/passfort.error_response.types';
 export * from './types/OTS_CC_CheckResponse';
 export * from './types/passfort.signature_validation.types';
+export * from './types/OTS_CC_config';
 export * from './formatters/OTS_CC_helpers';
 export * from './formatters/validate_signature.helpers';
 export * from './filters/passfort.app_exception';
